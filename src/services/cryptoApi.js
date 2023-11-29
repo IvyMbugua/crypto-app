cacfc6c2d0812f282d85353a44168fdd05a142b4
Ivy-Wairimu <130651187+Ivy-Wairimu@users.noreply.github.com>
2023-11-29 16:57:42 +0300

    initial commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '53fbbc844dmsh4e66d58cc824f22p15b9b1jsn48332197a1a8',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+    endpoints:(builder) =>({
+        getCryptos:builder.query({
+            query:(count)=> createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails:builder.query({
+            query:(coinId)=> createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory:builder.query({
+            query:({coinId, timePeriod })=> createRequest(`coin/${coinId}/history?timePeriod=${timePeriod}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        }),
+    }),
+});
+
+export const {
+    useGetCryptosQuery, 
+    useGetCryptoDetailsQuery, 
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
+
+} = cryptoApi;
